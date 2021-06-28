3ae81aa1efdffb3789485d1b973cc5fc8f3a67df
ukalelei <75921243+ukalelei@users.noreply.github.com>
2021-06-28 14:22:00 -0700

    -

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { analyzeAndValidateNgModules } from '@angular/compiler';
 import { Component } from '@angular/core';
 import { Satellite } from './satellite';
 
@@ -13,16 +14,38 @@ export class AppComponent {
   sourceList: Satellite[];
 
   constructor() {
-   this.sourceList = [
-      new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
-      new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
-      new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
-      new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
-      new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
-   ];
+    this.sourceList = [];
+    this.satelliteList = []
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+    
+    window.fetch(this.satellitesUrl).then(function(response) {
+      response.json().then(function(data) {
+        let fetchedSatellites = data.satellites;
+        
+        for (let i = 0; i < fetchedSatellites.length; i++){
+          let satellite = new Satellite(
+            fetchedSatellites[i].name,
+            fetchedSatellites[i].type,
+            fetchedSatellites[i].launchDate,
+            fetchedSatellites[i].orbitType,
+            fetchedSatellites[i].operational
+            )
+            this.sourceList.push(satellite);
+          } 
+          this.satelliteList= this.sourceList.slice(0);
+        }.bind(this));
+      }.bind(this));
+  }
 }
 
-}
+/*this.sourceList.push(
+   new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
+   new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
+   new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
+   new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
+   new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
+);*/
+
 
 
 
--- src/app/orbit-list/orbit-list.component.ts
+++ src/app/orbit-list/orbit-list.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Satellite } from '../satellite';
 
 @Component({
   selector: 'app-orbit-list',
   templateUrl: './orbit-list.component.html',
   styleUrls: ['./orbit-list.component.css']
 })
+
+
+
 export class OrbitListComponent implements OnInit {
 
-  constructor() { }
+  @Input() satellites!: Satellite[];
 
+  constructor() {
+  }
   ngOnInit(): void {
   }
 
